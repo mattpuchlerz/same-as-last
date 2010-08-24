c76b2d93c4a23c864111338100e445743de4c31d
Matt Puchlerz <matt@puchlerz.com>
2010-08-24 08:44:02 -0500

    Got the code flowin

--- spec/javascripts/same_as_last_spec.js
+++ spec/javascripts/same_as_last_spec.js
@@ -1,7 +1,33 @@
-describe('SameAsLast', function() {
+describe('Same as Last', function() {
   
-  it('exists', function() {
-    expect( SameAsLast ).not.toBe(null);
+  describe('when the game hasn’t yet started', function() {
+    
+    it('knows that it hasn’t yet started', function() {
+      expect( S.started ).toBe(false);
+    });
+    
+    it('has 20 cards', function() {
+      expect( S.cards.length ).toBe(20);
+    });
+    
+  });
+  
+  describe('when starting the game', function() {
+    
+    it('can be started', function() {
+      S.start();
+      expect( S.started ).toBe(true);
+    });
+    
+  });
+  
+  describe('when resetting', function() {
+    
+    it('changes the game to be not started', function() {
+      S.reset();
+      expect( S.started ).toBe(false);
+    });
+    
   });
   
 });
