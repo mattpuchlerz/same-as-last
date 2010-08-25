1e5f9d3516b40f4695cbf534ff9b82d19d29bfe2
Matt Puchlerz <matt@puchlerz.com>
2010-08-24 22:19:05 -0500

    Speced and implemented the values of cards when starting a game.

--- spec/javascripts/same_as_last_spec.js
+++ spec/javascripts/same_as_last_spec.js
@@ -1,3 +1,12 @@
+Array.prototype.include = function(val) {
+  for (var i = this.length - 1; i >= 0; i--){
+    if ( this[i] === val ) return true;
+  };
+  return false;
+};
+
+
+
 describe('Same as Last', function() {
   
   describe('when the game hasn’t yet started', function() {
@@ -26,19 +35,25 @@ describe('Same as Last', function() {
       expect( S.cards.length ).toBe(20);
     });
     
-    it('has a random value between 1 and 5 for each card', function() {
+    it('has a value between 0 and 3 for each card', function() {
       for (var i = S.cards.length - 1; i >= 0; i--) {
-        expect( S.cards[i] ).toBeWithin(1, 5);
+        expect( S.cards[i] ).toBeWithin(0, 3);
       }
     });
     
-  });
-  
-  describe('when resetting', function() {
+    it('has at least 1 card with each value', function() {
+      var values = [];
+      
+      for (var i = S.cards.length - 1; i >= 0; i--) {
+        if ( values.include( S.cards[i] ) ) continue;
+        values.push( S.cards[i] );
+      }
+      
+      expect( values.sort() ).toEqual([ 0, 1, 2, 3 ]);
+    });
     
-    it('changes the game to be not started', function() {
-      S.reset();
-      expect( S.started ).toBe(false);
+    it('does NOT begin with a fully successive section of cards', function() {
+      expect( S.cards.splice(0, 4) ).not.toEqual([ 0, 1, 2, 3 ]);
     });
     
   });
